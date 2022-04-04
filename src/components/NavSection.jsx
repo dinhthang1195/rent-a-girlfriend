fc5afbc2586a394302577a0bddb633a8162153c2
dinhthang1195 <dinhthang1195@gmail.com>
2022-04-04 18:19:50 +0700

    Update Navbar

--- src/components/NavSection.jsx
+++ src/components/NavSection.jsx
@@ -2,53 +2,53 @@ import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Logo from '../images/logo.png';
 import Button from 'react-bootstrap/Button';
+import Nav from 'react-bootstrap/Nav';
 import { Link } from 'react-router-dom';
 
 function NavSection() {
   return (
     <>
-      <Navbar sticky='top' className=' ml-0 py-4 '>
-        <div className='container'>
-          <div className='row justify-content-between'>
-            <div className='logo ps-0 pe-4   col pe-4 ps-1'>
-              <img src={Logo} alt='logo' />
-            </div>
-            <div className='  col pe-4 ps-1'>
-              <Link to='/cast' className=' border-end text-nowrap pe-4  text-decoration-none text-black '>
-                Girlfriend List
-              </Link>
-            </div>
-            <div className='  col pe-4 ps-1'>
-              <Link to='/price-plan' className=' border-end  text-nowrap pe-4  text-decoration-none text-black '>
-                Price / Date Plan
-              </Link>
-            </div>
-            <div className='  col pe-4 ps-1'>
-              <Link to='/usage' className=' border-end  text-nowrap pe-4  text-decoration-none text-black '>
-                How to use
-              </Link>
-            </div>
-            <div className='  col pe-4 ps-1'>
-              <Link to='/experience' className=' border-end  text-nowrap pe-4  text-decoration-none text-black '>
-                Customer Experience
-              </Link>
-            </div>
-            <div className='  col pe-4 ps-1'>
-              <Link to='/faq' className=' border-end  text-nowrap pe-4  text-decoration-none text-black '>
-                FAQ
-              </Link>
-            </div>
-
-            <div className=' col ps-1'>
-              <div>
-                <Button className=' btn-sign-in'>Sign In</Button>
-              </div>
-              <div>
-                <Button className=' btn btn-light '>Sign Up</Button>
-              </div>
-            </div>
+      <Navbar sticky='top' className='  py-4 '>
+        <Container>
+          <div className='logo '>
+            <img src={Logo} alt='logo' />
           </div>
-        </div>
+
+          <Nav.Link href='/cast' className=' border-end text-nowrap py-0 px-5 text-decoration-none text-black '>
+            Girlfriend List
+          </Nav.Link>
+
+          <Nav.Link
+            href='/price-plan'
+            className=' plan-price border-end  text-nowrap py-0 px-5  text-decoration-none text-black '
+          >
+            Price / Date Plan
+          </Nav.Link>
+
+          <Nav.Link href='/usage' className=' border-end  text-nowrap py-0 px-5   text-decoration-none text-black '>
+            How to use
+          </Nav.Link>
+
+          <Nav.Link
+            href='/experience'
+            className=' border-end  text-nowrap py-0 px-5   text-decoration-none text-black '
+          >
+            Customer Experience
+          </Nav.Link>
+
+          <Nav.Link href='/faq' className=' border-end  text-nowrap py-0 px-5   text-decoration-none text-black '>
+            FAQ
+          </Nav.Link>
+
+          <div className=' d-flex ms-3'>
+            <span className='  '>
+              <button className=' btn col-auto text-nowrap btn-sign-in me-1 '>Sign In</button>
+            </span>
+            <span className=''>
+              <button className=' btn col-auto text-nowrap btn-light '>Sign Up</button>
+            </span>
+          </div>
+        </Container>
       </Navbar>
     </>
   );
